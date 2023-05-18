29b3a4187efd396c1891101b202d51729cd4cf07
Luciano Wayand de Abreu <36968613+lucianowayand@users.noreply.github.com>
2023-05-18 11:29:13 -0300

    refactor: improving responsive conditional rendering

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -21,77 +21,46 @@ export default function Login() {
     }
 
     return <Box display="flex" height="100vh" width="100vw">
-        {width > 1000 ?
-            <>
-                <Box
-                    width="50%"
-                    padding="3em"
-                    display="flex"
-                    alignItems="center"
-                    justifyContent="center"
-                    sx={{ background: "#dce0e2" }}
-                >
-                    <Card sx={{ padding: "2em" }}>
-                        <Box padding="2em" display="flex" flexDirection="column" alignItems="center">
-                            <Typography variant="h4" fontFamily="Inter" fontWeight={600} mb={3}>LOGIN</Typography>
-                            <Box onSubmit={submitForm} component="form" display="flex" flexDirection="column" alignItems="center">
-                                <Box marginTop="3em">
-                                    <Typography variant="body1" fontFamily="Inter" fontWeight={400}>Email</Typography>
-                                    <input required type="email" value={email} onChange={(event) => setEmail(event.target.value)} className="input" />
-                                </Box>
-                                <Box marginTop="1.5em">
-                                    <Typography variant="body1" fontFamily="Inter" fontWeight={400}>Password</Typography>
-                                    <input required value={password} onChange={(event) => setPassword(event.target.value)} type="password" className="input" />
-                                </Box>
-                                <Box marginTop="3em">
-                                    <button className="button">Login</button>
-                                </Box>
-                            </Box>
+        <Box
+            width={width > 1000 ? "50%" : "100%"}
+            padding="3em"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            sx={{ background: width > 1000 ? "#dce0e2" : undefined }}
+            className={width < 1000 ? "background-gradient" : undefined}
+        >
+            <Card sx={{ padding: "2em" }}>
+                <Box padding="2em" display="flex" flexDirection="column" alignItems="center">
+                    <Typography variant="h4" fontFamily="Inter" fontWeight={600} mb={3}>LOGIN</Typography>
+                    <Box onSubmit={submitForm} component="form" display="flex" flexDirection="column" alignItems="center">
+                        <Box marginTop="3em">
+                            <Typography variant="body1" fontFamily="Inter" fontWeight={400}>Email</Typography>
+                            <input required type="email" value={email} onChange={(event) => setEmail(event.target.value)} className="input" />
                         </Box>
-                    </Card>
-                </Box>
-                <Box
-                    width="50%"
-                    height="100vh"
-                    className="background-gradient"
-                    display="flex"
-                    alignItems="center"
-                    justifyContent="center"
-                    flexDirection="column"
-                >
-                    <Typography variant="h1" color="white" fontFamily="Inter" fontWeight={600}>Moovy</Typography>
-                    <Typography color="white" fontFamily="Inter" fontWeight={400} fontSize={21}>Your movies, always with you.</Typography>
-                </Box>
-            </>
-            : <>
-                <Box
-                    width="100%"
-                    height="100vh"
-                    className="background-gradient"
-                    display="flex"
-                    alignItems="center"
-                    justifyContent="center"
-                    flexDirection="column"
-                >
-                    <Card sx={{ padding: "2em" }}>
-                        <Box padding="2em" display="flex" flexDirection="column" alignItems="center">
-                            <Typography variant="h4" fontFamily="Inter" fontWeight={600} mb={3}>LOGIN</Typography>
-                            <Box onSubmit={submitForm} component="form" display="flex" flexDirection="column" alignItems="center">
-                                <Box marginTop="3em">
-                                    <Typography variant="body1" fontFamily="Inter" fontWeight={400}>Email</Typography>
-                                    <input required type="email" value={email} onChange={(event) => setEmail(event.target.value)} className="input" />
-                                </Box>
-                                <Box marginTop="1.5em">
-                                    <Typography variant="body1" fontFamily="Inter" fontWeight={400}>Password</Typography>
-                                    <input required value={password} onChange={(event) => setPassword(event.target.value)} type="password" className="input" />
-                                </Box>
-                                <Box marginTop="3em">
-                                    <button className="button">Login</button>
-                                </Box>
-                            </Box>
+                        <Box marginTop="1.5em">
+                            <Typography variant="body1" fontFamily="Inter" fontWeight={400}>Password</Typography>
+                            <input required value={password} onChange={(event) => setPassword(event.target.value)} type="password" className="input" />
                         </Box>
-                    </Card>
+                        <Box marginTop="3em">
+                            <button className="button">Login</button>
+                        </Box>
+                    </Box>
                 </Box>
-            </>}
-    </Box>
+            </Card>
+        </Box>
+        {width > 1000 ? <Box
+            width="50%"
+            height="100vh"
+            className="background-gradient"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            flexDirection="column"
+        >
+            <Typography variant="h1" color="white" fontFamily="Inter" fontWeight={600}>Moovy</Typography>
+            <Typography color="white" fontFamily="Inter" fontWeight={400} fontSize={21}>Your movies, always with you.</Typography>
+        </Box> : null}
+
+    </Box >
 }
